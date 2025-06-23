dd4ab2ea41adf0801bcd5f47df6a94bd49f6eb8a
Smy250 <samyhopsu250@gmail.com>
2025-06-23 13:15:17 -0400

    feat(src/routes/categories): added get categories by id functionality

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,4 +1,7 @@
 import { Hono } from "hono";
+import { db } from "@/db";
+import * as schema from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 const app = new Hono();
 
@@ -6,8 +9,28 @@ app.get("/", (c) => {
   return c.json({});
 });
 
-app.get("/:id", (c) => {
-  return c.json({});
+app.get("/:id", async (c) => {
+  const id = c.req.param("id");
+  const categoryId = parseInt(id, 10);
+
+  if (isNaN(categoryId)) {
+    return c.json({ error: "invalid category ID" }, 400);
+  }
+
+  const categoryData = await db.query.categories.findFirst({
+    where: eq(schema.categories.id, categoryId)
+  });
+
+  if (!categoryData) {
+    return c.json({ error: "category ID not found" }, 400);
+  }
+
+  const response = {
+    id: categoryData.id,
+    name: categoryData.name
+  }
+
+  return c.json(response);
 });
 
 app.get("/:id/subcategories", (c) => {

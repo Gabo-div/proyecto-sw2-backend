f7d539b3817fcd8d74d9172169a58df0e6d69b5d
NetKBs <abijose535@gmail.com>
2025-06-24 19:09:03 -0400

    feat: endpoints

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -3,16 +3,29 @@ import { categories, subcategories } from './schema'
 
 const seed = async () => {
     // Clean up
-    await db.delete(categories)
     await db.delete(subcategories)
+    await db.delete(categories)
+    
 
     const insertedCategories = await db.insert(categories).values([
         { name: "Muebles" },
         { name: "Cocina" },
         { name: "Decoración" },
+        { name: "Banos" },
+        { name: "Dormitorios" },
+        { name: "Sala de estar" },
+        { name: "Jardines" },
     ]).returning()
 
-    const [furniture, kitchen, decoration] = insertedCategories
+    const [
+        furniture,
+        kitchen,
+        decoration,
+        bathroom,
+        bedroom,
+        livingroom,
+        garden,
+    ] = insertedCategories
 
     await db.insert(subcategories).values([
         { name: "Mesas", categoryId: furniture.id },
@@ -20,6 +33,10 @@ const seed = async () => {
         { name: "Electrodomesticos", categoryId: kitchen.id },
         { name: "Floreros", categoryId: decoration.id },
         { name: "Lamparas", categoryId: decoration.id },
+        { name: "Ba os", categoryId: bathroom.id },
+        { name: "Camas", categoryId: bedroom.id },
+        { name: "Sofas", categoryId: livingroom.id },
+        { name: "Plantas", categoryId: garden.id },
     ]).returning()
 }
 
--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,17 +1,89 @@
 import { Hono } from "hono";
+import { db } from "@/db";
+import { categories, subcategories } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.json({});
+app.get("/", async (c) => {
+  try {
+    const page = c.req.query("page");
+    const pageSize = c.req.query("pageSize");
+
+    const pageNumber = Number(page) || 1;
+    const pageSizeNumber = Number(pageSize) || 10;
+
+    const offset = (pageNumber - 1) * pageSizeNumber;
+
+    const result = await db
+      .select({
+        id: categories.id,
+        name: categories.name,
+      })
+      .from(categories)
+      .limit(pageSizeNumber)
+      .offset(offset)
+      .all();
+
+    const response = {
+      page: pageNumber,
+      pageSize: pageSizeNumber,
+      total: result.length,
+      data: result,
+    };
+    return c.json({ response });
+  } catch (error) {
+    console.error("Failed to fetch all categories:", error);
+    c.status(500);
+    return c.json({ error: "An internal server error occurred" });
+  }
 });
 
 app.get("/:id", (c) => {
   return c.json({});
 });
 
-app.get("/:id/subcategories", (c) => {
-  return c.json({});
+app.get("/:id/subcategories", async (c) => {
+  try {
+    const id = c.req.param("id");
+    const categoryId = parseInt(id, 10);
+
+    if (isNaN(categoryId)) {
+      return c.json({ error: "Invalid category ID" }, 400);
+    }
+
+    const page = c.req.query("page");
+    const pageSize = c.req.query("pageSize");
+
+    const pageNumber = Number(page) || 1;
+    const pageSizeNumber = Number(pageSize) || 10;
+
+    const offset = (pageNumber - 1) * pageSizeNumber;
+
+    const result = await db
+      .select({
+        id: subcategories.id,
+        name: subcategories.name,
+      })
+      .from(subcategories)
+      .where(eq(subcategories.categoryId, categoryId))
+      .limit(pageSizeNumber)
+      .offset(offset)
+      .all();
+
+    const response = {
+      page: pageNumber,
+      pageSize: pageSizeNumber,
+      total: result.length,
+      data: result,
+    };
+
+    return c.json({ response });
+  } catch (error) {
+    console.error("Failed to fetch all categories:", error);
+    c.status(500);
+    return c.json({ error: "An internal server error occurred" });
+  }
 });
 
 export default app;
